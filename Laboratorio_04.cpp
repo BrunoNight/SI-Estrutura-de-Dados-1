637b2db2e5a7d479c0ed0b97b709342b03ee47e7
BrunoNight <brunobrrt3680@gmail.com>
2026-05-14 09:16:28 -0300

    Laboratorio_04.cpp

--- Laboratorio_04.cpp
+++ Laboratorio_04.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
+#include <string.h>
 
-using namespace std
+using namespace std;
 
 class Nodo { public:
-    int info;
+    char info;
     Nodo *prox;
-}
+};
 
 class Lista {
     private:
@@ -15,85 +16,30 @@ class Lista {
             inicio = nullptr;
         }
         int tamanho();
-        void append(valor);
-        void inserePos(valor, pos);
-        void retirar(valor);
-        void ordena();
-        void imprime();
+        void adicionar(int valor);
+        void inserePos(int valor, int pos);
+        void retirar(int valor);
+        void ordenar();
+        void imprimir();
 }
 
-void Lista :: append(int valor) {
-    Nodo *novo = new Nodo;
-    
-    if(novo == nullptr) {
-        cout << "Espaço não criado!" << endl;
-    }
-    
-    novo -> info = valor;
-    novo -> prox = nullptr;
+int Lista::tamanho() {
+    int qtNum = 0;
+    Nodo *atual;
     
     if(inicio == nullptr) {
-        inicio = novo;
-        return;
+        return qtNum;
     }
     
-    inicio -> prox = novo;
-    return;
-}
-
-//void Lista :: retirar(int valor) {
-    if(inicio == nullptr) {
-        cout << "Lista vazia!" << endl;
-        return;
-    }
+    atual = inicio;
     
-    Nodo *aux;
-    
-    while(inicio != nullptr) {
-        if(valor == inicio -> info) {
-            aux = inicio;
-            inicio -> prox = prox;
-            
+    while(atual != nullptr) {
+        if(atual == nullptr) {
+            break;
         }
+        qtNum++;
+        atual = atual -> prox;
     }
     
+    return qtNum;
 }
-
-void Lista :: ordenar() {
-    if(inicio == nullptr) {
-        cout << "Lista vazia!" << endl;
-        return;
-    }
-    
-    Nodo *ant, *atual, *aux, *iniX = nullptr;
-    
-    while(inicio != nullptr) {
-        aux = inicio;
-        inicio = inicio -> prox;
-        atual = iniX;
-        if(iniX == nullptr) {
-            iniX = aux;
-            aux -> prox = nullptr
-        } else {
-            while(atual != nullptr) {
-                ant = atual;
-                atual = atual -> prox;
-            }
-        }
-        
-        if(atual == iniX) {
-            aux -> prox = iniX;
-            iniX = aux;
-        } else if(atual == nullptr) {
-            ant -> prox = aux;
-            aux -> prox = nullptr;
-        } else {
-            ant -> prox = aux;
-            aux -> prox = atual;
-        }
-    }
-}
-
-//int Lista :: tamanho() {
-    
-//}
