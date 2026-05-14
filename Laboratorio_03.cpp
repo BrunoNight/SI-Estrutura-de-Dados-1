fdb36904d67465e036e72ecbafa897ddcb1cfc53
BrunoNight <brunobrrt3680@gmail.com>
2026-05-14 08:31:23 -0300

    Laboratorio_03.cpp

--- Laboratorio_03.cpp
+++ Laboratorio_03.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class Nodo {
+class Nodo { public:
     char info;
     Nodo *prox;
 };
@@ -17,6 +17,12 @@ class Pilha {
         }
         void adicionar(char letra);
         char remover();
+        bool vazio() {
+            if(topo == nullptr) {
+                return true;
+            }
+            return false;
+        }
 };
 
 class Fila {
@@ -30,30 +36,36 @@ class Fila {
         }
         void adicionar(char letra);
         char remover();
+        bool vazio() {
+            if(inicio == nullptr) {
+                return true;
+            }
+            return false;
+        }
 };
 
 void Pilha::adicionar(char letra) {
     Nodo *novoP = new Nodo;
-    
+
     if(novoP == nullptr) {
         cout << "Pilha não criada" << endl;
         exit(1);
     }
-    
+
     novoP -> info = letra;
-    novoP -> prox = nullptr;
+    novoP -> prox = topo;
     topo = novoP;
 }
 
 char Pilha::remover() {
     char letraRem;
     Nodo *aux;
-    
+
     if(topo == nullptr) {
         cout << "Pilha vazia!" << endl;
         exit(1);
     }
-    
+
     aux = topo;
     letraRem = aux -> info;
     topo = aux -> prox;
@@ -63,20 +75,20 @@ char Pilha::remover() {
 
 void Fila::adicionar(char letra) {
     Nodo *novoF = new Nodo;
-    
-    if(novoP == nullptr) {
+
+    if(novoF == nullptr) {
         cout << "Pilha não criada" << endl;
         exit(1);
     }
-    
+
     novoF -> info = letra;
     novoF -> prox = nullptr;
-    
+
     if(inicio == nullptr && fim == nullptr) {
         inicio = fim = novoF;
         return;
     }
-    
+
     fim -> prox = novoF;
     fim = novoF;
 }
@@ -84,20 +96,20 @@ void Fila::adicionar(char letra) {
 char Fila::remover() {
     char letraRem;
     Nodo *aux;
-    
+
     if(inicio == nullptr) {
         cout << "Fila vazia!" << endl;
         exit(1);
     }
-    
+
     aux = inicio;
     letraRem = aux -> info;
     inicio = aux -> prox;
-    
+
     if(inicio == nullptr) {
         fim = nullptr;
     }
-    
+
     delete aux;
     return letraRem;
 }
@@ -107,15 +119,32 @@ bool palindromo(Pilha &P, Fila &F, string palavra) {
         P.adicionar(c); // Última letra a entrar é a primeira a sair
         F.adicionar(c); // Primeira letra a entrar é a primeira a sair
     }
-    
-    while(P != nullptr && F != nullptr) {
+
+    while(!P.vazio() && !F.vazio()) {
         char letraCompP = P.remover();
         char letraCompF = F.remover();
-        
+
         if(letraCompP != letraCompF) {
             return false;
         }
     }
-    
+
     return true;
+}
+
+int main() {
+    Pilha P;
+    Fila F;
+    string palavra;
+    
+    cout << "Digite palavra: ";
+    cin >> palavra;
+    
+    if(palindromo(P, F, palavra)) {
+        cout << "É palíndromo";
+    } else {
+        cout << "Não é palíndromo";
+    }
+    
+    return 0;
 }
